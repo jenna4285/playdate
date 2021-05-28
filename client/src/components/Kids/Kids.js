e374e164b79603e14f6686d877e90660743f87d8
Jenna Garrison <78498827+jenna4285@users.noreply.github.com>
2021-05-28 13:04:37 -0500

    signup form and starter for kid component and login w/ pseudocode

---
+++ client/src/components/Kids/Kids.js
@@ -0,0 +1,15 @@
+// import React from "react";
+// import { MultiSelect } from 'primereact/multiselect';
+
+// // function Kids() {
+// //     return(
+// //         // Child first name
+// //         // Child Age
+// //         // Multi-select kid interests
+// //         // button to add another kid
+// //         // button for complete with family
+
+// //     )
+// // }
+
+// export default Kids;
