73ae571c1293019cf72e590a16cb82bec278640f
Jenna Garrison <78498827+jenna4285@users.noreply.github.com>
2021-05-28 18:10:02 -0500

    Kids, Profile, and Signup forms populated

--- client/src/components/Kids/Kids.js
+++ client/src/components/Kids/Kids.js
@@ -1,15 +1,32 @@
-// import React from "react";
+import React from "react";
+import { InputText } from 'primereact/inputtext';
 // import { MultiSelect } from 'primereact/multiselect';
 
-// // function Kids() {
-// //     return(
-// //         // Child first name
-// //         // Child Age
-// //         // Multi-select kid interests
-// //         // button to add another kid
-// //         // button for complete with family
+function Kids() {
+    return (
+        <form className="container-fluid">
+            <h2>Kid Info!</h2>
 
-// //     )
-// // }
+            <div className="p-fluid p-formgrid p-grid">
+                <div className="p-field p-col-6 p-md-6">
+                    <label htmlFor="kidname">Kiddo Name</label>
+                    <InputText id="kidname" type="text" />
+                </div>
+                <div className="p-field p-col-6 p-md-6">
+                    <label htmlFor="kidage">Kiddo Age</label>
+                    <InputText id="kidage" type="integer" />
+                 </div>
+                 </div>
+                 {/* Kiddo interest for matching in the future */}
+            {/* <div className="p-fluid p-formgrid p-grid" */}
+            <button type="button" class="btn btn-primary btn-lg px-4 gap-3" id="addchild">Add Child</button>
+            <br></br>
+            <button type="button" class="btn btn-primary btn-lg px-4 gap-3" id="familycomplete">Family Complete</button>
+        </form>
 
-// export default Kids;
+
+
+    )
+}
+
+export default Kids;
--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,33 +1,36 @@
-// import React from "react";
-// import 'primeflex/primeflex.css';
-// import Kids from "./Kids";
-// import { InputText } from 'primereact/inputtext';
-// import {selectedState, states, onStateChange, Dropdown, InputTextarea} from 'primereact';
+import React from "react";
+import 'primeflex/primeflex.css';
+import Kids from "../Kids/Kids";
+import { InputText } from 'primereact/inputtext';
+import { InputTextarea } from 'primereact/inputtextarea';
 
 
-// function Profile(){
-//     return (
-//     <div>
-//         <div className="p-field p-col-12">
-//         <label htmlFor="address">Street Address</label>
-//         <InputTextarea id="address" type="text" rows="4" />
-//     </div>
-//     <div className="p-field p-col-12 p-md-6">
-//         <label htmlFor="city">City</label>
-//         <InputText id="city" type="text" />
-//     </div>
-//     <div className="p-field p-col-12 p-md-3">
-//         <label htmlFor="state">State</label>
-//         <Dropdown inputId="state" value={selectedState} options={states} onChange={onStateChange} placeholder="Select" optionLabel="name"/>
-//     </div>
-//     <div className="p-field p-col-12 p-md-3">
-//         <label htmlFor="zip">Zip</label>
-//         <InputText id="zip" type="text" />
-//     </div>
-//     <Kids/>
-//    </div> 
-//  )
-// }
+function Profile() {
+    return (
+        <form className="container-fluid">
+            <h1>Profile Information:</h1><br></br>
+            <div>
+                <div className="p-field p-col-12">
+                    <label htmlFor="address">Street Address</label>
+                    <InputTextarea id="address" type="text" rows="1" />
+                </div>
+                <div className="p-field p-col-12 p-md-6">
+                    <label htmlFor="city">City</label>
+                    <InputText id="city" type="text" />
+                </div>
+                <div className="p-field p-col-6 p-md-6">
+                    <label htmlFor="state">State</label>
+                    <InputText id="state" type="text" />
+                </div>
+                <div className="p-field p-col-6 p-md-3">
+                    <label htmlFor="zip">Zip</label>
+                    <InputText id="zip" type="text" />
+                </div>
+                <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
+               <Kids />
+            </div>
+        </form>
+    )
+}
 
-
-// export default Profile;
+export default Profile;
