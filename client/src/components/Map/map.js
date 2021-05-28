06a7cd7db25c8a440ad7c3186d9f8c65516395ef
Jenna Garrison <78498827+jenna4285@users.noreply.github.com>
2021-05-28 11:25:24 -0500

    stash for pull

---
+++ client/src/components/Map/map.js
@@ -0,0 +1,14 @@
+// import React from "react";
+// import "./style.css";
+
+// let map;
+
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 8,
+//   });
+// }
+
+
+// export default Map;
