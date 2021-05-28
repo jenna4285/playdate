b97d4a592677ea33915bd684e74645603ea26e35
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-05-28 14:44:47 -0500

    added 4 blank pages and routed in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
 // import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
@@ -13,6 +17,10 @@ function App() {
         <Nav />
         <Switch>
           <Route exact path={["/", "/home"]} component={Home}/>
+          <Route exact path="/profile" component={Profile}/>
+          <Route exact path="/dashboard" component={Dashboard}/>
+          <Route exact path="/signup" component={Signup}/>
+          <Route exact path="/login" component={Login}/>
           <Route exact path="/books/:id" component={Detail}/>
           <Route>
             <NoMatch />
