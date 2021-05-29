e861210027baa7321115e48e6bbc65747582c188
Ian Darland <iansmbpro@ians-mbp.lan>
2021-05-28 20:19:07 -0500

    stashing before pull

--- client/src/components/SignupForm/SignupForm.js
+++ client/src/components/SignupForm/SignupForm.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import { InputText } from 'primereact/inputtext';
 
 function SignupForm() {
+
+    const [signUpInfo, setSignUpInfo] = useState({
+        firstname: "",
+        lastname: ""
+    })
+
+    function handleInputChange(event){
+        const {name, value} = event.target
+        setSignUpInfo(...signUpInfo, [name], value)
+        console.log(signUpInfo)
+    }
+
     return(
-        
+    
 <div className="p-fluid p-formgrid p-grid">
     <div className="p-field p-col-12 p-md-6">
         <label htmlFor="firstname6">Firstname</label>
-        <InputText id="firstname6" type="text" />
+        <InputText id="firstname6" name="firstname" value={signUpInfo.firstname} onChange={handleInputChange}  type="text" />
     </div>
     <div className="p-field p-col-12 p-md-6">
         <label htmlFor="lastname6">Lastname</label>
-        <InputText id="lastname6" type="text" />
+        <InputText id="lastname6" type="text" name="lastname" value={signUpInfo.lastname} onChange={handleInputChange} />
     </div>
      <div className="p-field p-col-12 p-md-6">
         <label htmlFor="lastname6">Email</label>
@@ -21,6 +33,7 @@ function SignupForm() {
         <label htmlFor="password6">Password</label>
         <InputText id="password" type="password" />
 </div>
+
 </div>
  
     )
