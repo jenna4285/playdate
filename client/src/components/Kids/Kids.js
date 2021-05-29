9a6d3663daebe07d83508776f15e14e278438632
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-05-29 02:42:04 -0500

    formatted edit profile page. Added 50 united states to unitedstates.json as an object. imported into Profileform.js. States are now dropdown selection. Changed form button colors.

--- client/src/components/Kids/Kids.js
+++ client/src/components/Kids/Kids.js
@@ -1,27 +1,38 @@
 import React from "react";
 import { InputText } from 'primereact/inputtext';
+import './Kids.css'
 // import { MultiSelect } from 'primereact/multiselect';
 
 function Kids() {
     return (
         <form className="container-fluid">
-            <h2>Kid Info!</h2>
+            <h1>Kids</h1>
 
             <div className="p-fluid p-formgrid p-grid">
-                <div className="p-field p-col-6 p-md-6">
-                    <label htmlFor="kidname">Kiddo Name</label>
-                    <InputText id="kidname" type="text" />
+                <div className="p-field p-col-12 p-md-6">
+                    <h5 htmlFor="child-name">Name</h5>
+                    <div className="p-inputgroup">
+                        <span className="p-inputgroup-addon">
+                            <i className="pi pi-user"></i>
+                        </span>
+                        <InputText id="child-name" type="text" />
+                    </div>
                 </div>
-                <div className="p-field p-col-6 p-md-6">
-                    <label htmlFor="kidage">Kiddo Age</label>
-                    <InputText id="kidage" type="integer" />
-                 </div>
-                 </div>
-                 {/* Kiddo interest for matching in the future */}
+                <div className="p-field p-col-12 p-md-6">
+                    <h5 htmlFor="child-age">Age</h5>
+                    <div className="p-inputgroup">
+                        <span className="p-inputgroup-addon">
+                            <i className="pi pi-palette"></i>
+                        </span>
+                        <InputText id="child-age" type="text" />
+                    </div>
+                </div>
+            </div>
+            {/* Kiddo interest for matching in the future */}
             {/* <div className="p-fluid p-formgrid p-grid" */}
-            <button type="button" class="btn btn-primary btn-lg px-4 gap-3" id="addchild">Add Child</button>
+            <button type="button" class="btn btn-success m-2 px-4 gap-3" id="addchild">Add Child</button>
             <br></br>
-            <button type="button" class="btn btn-primary btn-lg px-4 gap-3" id="familycomplete">Family Complete</button>
+            <button type="button" class="btn btn-success m-2 px-4 gap-3" id="familycomplete">Family Complete</button>
         </form>
 
 
