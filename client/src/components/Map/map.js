a164ffa92b1a23ae3692a4a060bc32b2ce33fcec
Jenna Garrison <78498827+jenna4285@users.noreply.github.com>
2021-05-30 11:59:35 -0500

    correct map

--- client/src/components/Map/map.js
+++ client/src/components/Map/map.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
 import { withGoogleMap, Map, GoogleApiWrapper,} from 'google-maps-react';
-// import withScriptjs from 'react-google-maps/lib/async/withScriptjs';
 
 const mapStyles = {
   width: '100%',
   height: '100%'
 };
 
-// const myMap = withScriptjs(withGoogleMap((props) => (<GoogleMap />)));
-
 export class MapContainer extends Component {
   render() {
     return (
       <Map
         google={this.props.google}
-        zoom={8}
+        zoom={14}
         style={mapStyles}
         initialCenter={
           {
@@ -28,5 +25,5 @@ export class MapContainer extends Component {
 }
 
 export default GoogleApiWrapper({
-  apiKey: `${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`
+  apiKey: 
 })(MapContainer);
