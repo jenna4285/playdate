1812cc913a8cd4bc8e47efcd12b816ce90f64d37
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-06-01 16:57:35 -0500

    added api call to 'your friends' component

--- client/src/components/YourFriends/YourFriends.js
+++ client/src/components/YourFriends/YourFriends.js
@@ -1,16 +1,26 @@
 import React, { useState, useEffect, useContext } from "react";
+import API from "../../utils/API";
+
 function YourFriends(props) {
 
+    const getFriends=async ()=>{
+        const friends=await API.getUsers()
+            console.log("My Friends");
+            console.log(friends);
+        return friends; 
+    }
+
+    useEffect(() => {
+        getFriends();
+    })
 
 
     return (
-
         <div className="col-sm-12 col-md-6 col-lg-6">
             <div className="card">
                 <h1>Your Friends</h1>
             </div>
         </div>
-
     )
 
 
