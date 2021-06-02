571a90b978dfb69a6d1c0197e99e6ff44c9a291b
Jenna Garrison <78498827+jenna4285@users.noreply.github.com>
2021-06-02 14:03:36 -0500

    activity form

--- client/src/components/ActivityForm/ActivityForm.js
+++ client/src/components/ActivityForm/ActivityForm.js
@@ -1,180 +1,148 @@
-// import React, { useState, useEffect, useContext } from "react";
-// import API from "../../utils/API"
-// import { InputText } from 'primereact/inputtext';
-// import { InputTextarea } from 'primereact/inputtextarea';
-// import { Dropdown } from 'primereact/dropdown';
-// import { useAuth0 } from "@auth0/auth0-react";
-// import unitedStates from './unitedstates';
-// import 'primeflex/primeflex.css';
-// import './Profileform.css'
-// import Geocode from "react-geocode";
-
-// Geocode.setApiKey("AIzaSyAQACrt018ybMocp5ofJnmPmB7XPiX23Yg");
+import React, { useState, useEffect, useContext } from "react";
+import API from "../../utils/API"
+import { InputText } from 'primereact/inputtext';
+import { InputTextarea } from 'primereact/inputtextarea';
+import { Dropdown } from 'primereact/dropdown';
+import { Calendar } from 'primereact/calendar';
+import { useAuth0 } from "@auth0/auth0-react";
+import 'primeflex/primeflex.css';
+import Geocode from "react-geocode";
+import AutoAddress from "./Autoaddress"
 
 
-// function ActivityForm() {
-
-//     const { isAuthenticated, user } = useAuth0();
-
-//     const [activityInfo, setActivityInfo] = useState({
-//         // Grabbing user.email from auth0
-//         // email:"",
-//         hostName: "",
-//         date: "",
-//         description: "",
-//         location: "",
-//         lat: "",
-//         lng: ""
-
-//     });
+Geocode.setApiKey("AIzaSyAQACrt018ybMocp5ofJnmPmB7XPiX23Yg");
 
 
-//     function saveToDatabase(){
-//         if (activityInfo.hostName && activityInfo.description && activityInfo.location && activityInfo.date) {
-            
-//             API.editActivityByEmail({
-//                 //GRABBING INFO FROM STATE
-//                 hostName: "",
-//                 date: "",
-//                 description: "",
-//                 location: "",
-//                 lat: profileInfo.lat,
-//                 lng: profileInfo.lng,
-//                 })
-//                 // .then(() => setProfileInfo({
-//                 //     username: "",
-//                 //     password: "",
-//                 // }))
-//                 .then(() => console.log("profile edited"))
-//                 // .then(() => window.location.href = "/dashboard")
-//                 .catch(err => console.log(err));
-//         }
-//     }
+function ActivityForm() {
 
-//     function handleBtnClick(event) {
-//         event.preventDefault();
-//         console.log(profileInfo.username)
-        
-//         const fullAddress = [profileInfo.address, profileInfo.city, profileInfo.unitedState, profileInfo.zip].join(",");
-        
-//         // pass full address to geohook to get lat lon for db
-//         let lat = "";
-//         let lng = "";
+    const { isAuthenticated, user } = useAuth0();
+
+    const [activityInfo, setActivityInfo] = useState({
+        // Grabbing user.email from auth0
+        // email:"",
+        hostName: "",
+        date: "",
+        description: "",
+        location: "",
+        lat: "",
+        lng: ""
+
+    });
 
 
-//         function latLon() {
-//             Geocode.fromAddress(fullAddress).then(
-//             (response) => {
-//               let { lat, lng } = response.results[0].geometry.location;
-//               setProfileInfo({...profileInfo, lat, lng});
-              
-//             },
-//             (error) => {
-//               console.error(error);
-//             }
-//           )
-//         };
-//         latLon();    
-//         console.log(lat, lng);
-        
-//         saveToDatabase();
-      
-//     }
+    function saveToDatabase() {
+        if (activityInfo.hostName && activityInfo.description && activityInfo.location && activityInfo.date) {
 
-//     useEffect(() => {
+            API.editActivityByEmail({
+                //GRABBING INFO FROM STATE
+                hostName: "",
+                date: "",
+                description: "",
+                location: "",
+                lat: activityInfo.lat,
+                lng: activityInfo.lng,
+            })
+                // .then(() => setProfileInfo({
+                //     username: "",
+                //     password: "",
+                // }))
+                .then(() => console.log("activity added"))
+                // .then(() => window.location.href = "/dashboard")
+                .catch(err => console.log(err));
+        }
+    }
 
-//     })
+    function handleBtnClick(event) {
+        event.preventDefault();
+        console.log(activityInfo.username)
 
-//     function handleInputChange(event) {
-//         const { name, value } = event.target
-//         setProfileInfo({ ...profileInfo, [name]: value })
-//     }
+        // const fullAddress = [profileInfo.address, profileInfo.city, profileInfo.unitedState, profileInfo.zip].join(",");
 
-
-//     return (
-
-//             <div className="row">
-//                 <div className="col">
-//                     <form>
-//                         <br />
-//                         <div>
-//                             <div className="card">
-//                                 <h1>Edit Main User/Family Profile</h1><br />
-//                                 <div className="card-body">
-//                                     <div className="row mb-3">
-//                                         <div className="col-sm-3">
-//                                             <h6 className="mb-0">Full Name</h6>
-//                                         </div>
-//                                         <div className="col-sm-9 text-secondary">
-//                                             <InputText name="fullname" type="text" className="form-control" value={profileInfo.name} id="profilename" onChange={handleInputChange} placeholder="Enter Name Here"/>
-//                                         </div>
-//                                     </div>
-//                                     <div className="row mb-3">
-//                                         <div className="col-sm-3">
-//                                             <h6 className="mb-0">Street Address</h6>
-//                                         </div>
-//                                         <div className="col-sm-9 text-secondary">
-//                                             <InputText name="address" type="text" className="form-control" value={profileInfo.address} id="profilestreetaddress" onChange={handleInputChange} />
-//                                         </div>
-//                                     </div>
-//                                     <div className="row mb-3">
-//                                         <div className="col-sm-3">
-//                                             <h6 className="mb-0">City</h6>
-//                                         </div>
-//                                         <div className="col-sm-9 text-secondary">
-//                                             <InputText name="city" type="text" className="form-control" value={profileInfo.city} id="profilecity" onChange={handleInputChange} />
-//                                         </div>
-//                                     </div>
-//                                     <div className="row mb-3">
-//                                         <div className="col-sm-3">
-//                                             <h6 className="mb-0">State</h6>
-//                                         </div>
-//                                         <div className="col-sm-9 text-secondary">
-//                                             <Dropdown name="unitedState" value={profileInfo.unitedState} options={unitedStates} onChange={handleInputChange} placeholder="Select a State" />
-//                                         </div>
-//                                     </div>
-//                                     <div className="row mb-3">
-//                                         <div className="col-sm-3">
-//                                             <h6 className="mb-0">Zipcode</h6>
-//                                         </div>
-//                                         <div className="col-sm-9 text-secondary">
-//                                             <InputText name="zip" type="text" className="form-control" value={profileInfo.zip} id="profilezip" onChange={handleInputChange}/>
-//                                         </div>
-//                                     </div>
-//                                     <div className="row mb-3">
-//                                         <div className="col-sm-3">
-//                                             <h6 className="mb-0">Bio</h6>
-//                                         </div>
-//                                         <div className="col-sm-9 text-secondary">
-//                                             <div className="p-inputgroup">
-//                                                 <span className="p-inputgroup-addon">
-//                                                     <i className="pi pi-users"></i>
-//                                                 </span>
-//                                                 <InputTextarea name="description" id="bio" rows={3} cols={35} value={profileInfo.description} onChange={handleInputChange} placeholder="Tell us a little about your family!" />
-//                                             </div>
-//                                             <div className="row">
-//                                                 <div className="col-sm-3"></div>
-//                                                 <div className="col-sm-9 text-secondary">
-//                                                 </div>
-//                                             </div>
-//                                         </div>
-
-//                                     </div>
-//                                     <div className="row">
-//                                         <button id="save-profile" type="button" className="btn btn-success px-4 gap-3"
-//                                             disabled={!(profileInfo.fullname && profileInfo.address && profileInfo.city && profileInfo.unitedState && profileInfo.zip && profileInfo.description)}
-//                                             onClick={handleBtnClick}>Save Profile</button>
-//                                     </div>
-
-//                                 </div>
-//                             </div>
-//                         </div>
-//                     </form>
-//                 </div>
-//             </div>
+        // pass full address to geohook to get lat lon for db
+        let lat = "";
+        let lng = "";
 
 
-//     )
-// }
+        function latLon() {
+            Geocode.fromAddress(activityInfo.fullAddress).then(
+                (response) => {
+                    let { lat, lng } = response.results[0].geometry.location;
+                    setActivityInfo({ ...activityInfo, lat, lng });
 
-// export default ActivityForm;
+                },
+                (error) => {
+                    console.error(error);
+                }
+            )
+        };
+        latLon();
+        console.log(lat, lng);
+
+        saveToDatabase();
+
+    }
+
+    useEffect(() => {
+
+    })
+
+    function handleInputChange(event) {
+        const { name, value } = event.target
+        setActivityInfo({ ...activityInfo, [name]: value })
+    }
+
+
+    return (
+
+        <div className="row">
+            <div className="col">
+                <form>
+                    <div>
+                        <div className="card">
+                            <h1>Add an Activity!</h1>
+                            <div className="row mb-3">
+                                <div className="col-sm-3">
+                                    <h6 className="mb-0">Event Location</h6>
+                                </div>
+                                <AutoAddress name="fullAddress" type="text" className="form-control" value={activityInfo.fullAddress} id="fullAddress" onChange={handleInputChange} />
+                            </div>
+                        </div>
+                        <div className="row mb-3">
+                            <div className="col-sm-3">
+                                <h6 className="mb-0">Event Description</h6>
+                            </div>
+                            <div className="col-sm-9 text-secondary">
+                                <div className="p-inputgroup">
+                                    <span className="p-inputgroup-addon">
+                                        <i className="pi pi-users"></i>
+                                    </span>
+                                    <InputTextarea name="description" id="bio" rows={3} cols={35} value={activityInfo.description} onChange={handleInputChange} placeholder="What are you up to?" />
+                                </div>
+                                <div className="row mb-3">
+                                    <div className="col-sm-3">
+                                        <h6 className="mb-0">Date</h6>
+                                    </div>
+                                    <Calendar />
+                                    <div className="row">
+                                        <div className="col-sm-3"></div>
+                                        <div className="col-sm-9 text-secondary">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <button id="save-activity" type="button" className="btn btn-success px-4 gap-3"
+                                        disabled={!(activityInfo.username && activityInfo.fullAddress && activityInfo.date && activityInfo.description)}
+                                        onClick={handleBtnClick}>Save Activity</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+
+    )
+}
+
+export default ActivityForm;
