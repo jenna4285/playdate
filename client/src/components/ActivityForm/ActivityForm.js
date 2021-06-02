6e820ecb15f7ae499ccf2b305b4e1e8532dc48c9
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-06-02 15:10:30 -0500

    styled button, formatted modal

--- client/src/components/ActivityForm/ActivityForm.js
+++ client/src/components/ActivityForm/ActivityForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext,useRef  } from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 import API from "../../utils/API"
 import { InputText } from 'primereact/inputtext';
 import { InputTextarea } from 'primereact/inputtextarea';
@@ -10,6 +10,7 @@ import Geocode from "react-geocode";
 import AutoAddress from "./Autoaddress"
 import { OverlayPanel } from 'primereact/overlaypanel';
 import { Button } from 'primereact/button';
+import './ActivityForm.css'
 
 
 Geocode.setApiKey("AIzaSyAQACrt018ybMocp5ofJnmPmB7XPiX23Yg");
@@ -99,53 +100,56 @@ function ActivityForm() {
 
         <div className="row">
             <div className="col">
-                <form>
-                    <div>
-                        <div className="card">
-                        <Button type="button" label="Basic" onClick={(e) => op.current.toggle(e)} />
-
-                                    <OverlayPanel ref={op}>
-                                        FORM GOES HERE
-                                    </OverlayPanel>
-                            <h1>Add an Activity!</h1>
-                            <div className="row mb-3">
-                                <div className="col-sm-3">
-                                    <h6 className="mb-0">Event Location</h6>
-                                </div>
-                                <AutoAddress name="fullAddress" type="text" className="form-control" value={activityInfo.fullAddress} id="fullAddress" onChange={handleInputChange} />
-                            </div>
-                        </div>
-                        <div className="row mb-3">
-                            <div className="col-sm-3">
-                                <h6 className="mb-0">Event Description</h6>
-                            </div>
-                            <div className="col-sm-9 text-secondary">
-                                <div className="p-inputgroup">
-                                    <span className="p-inputgroup-addon">
-                                        <i className="pi pi-users"></i>
-                                    </span>
-                                    <InputTextarea name="description" id="bio" rows={3} cols={35} value={activityInfo.description} onChange={handleInputChange} placeholder="What are you up to?" />
-                                </div>
+            <div className="d-flex justify-content-center">
+            {/* this button opens the modal */}
+                <Button id="add-activity-btn" type="button" label="Add an Activity!" onClick={(e) => op.current.toggle(e)} />
+            </div>
+            {/* what's in this OverlayPanel is hidden until toggled on/off */}
+                <OverlayPanel ref={op}>
+                    <form>
+                        <div>
+                            <div className="card">
                                 <div className="row mb-3">
                                     <div className="col-sm-3">
-                                        <h6 className="mb-0">Date</h6>
+                                        <h6 className="mb-0">Event Location</h6>
                                     </div>
-                                    <Calendar />
-                                    <div className="row">
-                                        <div className="col-sm-3"></div>
-                                        <div className="col-sm-9 text-secondary">
+                                    <AutoAddress name="fullAddress" type="text" className="form-control" value={activityInfo.fullAddress} id="fullAddress" onChange={handleInputChange} />
+                                </div>
+                            </div>
+                            <div className="row mb-3">
+                                <div className="col-sm-3">
+                                    <h6 className="mb-0">Event Description</h6>
+                                </div>
+                                <div className="col-sm-9 text-secondary">
+                                    <div className="p-inputgroup">
+                                        <span className="p-inputgroup-addon">
+                                            <i className="pi pi-users"></i>
+                                        </span>
+                                        <InputTextarea name="description" id="bio" rows={3} cols={35} value={activityInfo.description} onChange={handleInputChange} placeholder="What are you up to?" />
+                                    </div>
+                                    <div className="row mb-3">
+                                        <div className="col-sm-3">
+                                            <h6 className="mb-0">Date</h6>
+                                        </div>
+                                        <Calendar />
+                                        <div className="row">
+                                            <div className="col-sm-3"></div>
+                                            <div className="col-sm-9 text-secondary">
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div className="row">
-                                    <button id="save-activity" type="button" className="btn btn-success px-4 gap-3"
-                                        disabled={!(activityInfo.username && activityInfo.fullAddress && activityInfo.date && activityInfo.description)}
-                                        onClick={handleBtnClick}>Save Activity</button>
+                                    <div className="row">
+                                        <button id="save-activity" type="button" className="btn btn-success px-4 gap-3"
+                                            disabled={!(activityInfo.username && activityInfo.fullAddress && activityInfo.date && activityInfo.description)}
+                                            onClick={handleBtnClick}>Save Activity</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </OverlayPanel>
+
+
             </div>
         </div>
 
