a167e22c5ccc4e547e87df0064dce27dc2e29977
Jenna Garrison <78498827+jenna4285@users.noreply.github.com>
2021-06-02 10:54:24 -0500

    added grid back etc to correct delete cleanup and also started Activity Form work

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,20 +1,21 @@
 import React from "react";
-import { Col, Row, Container } from "../components/Grid";
-import Jumbotron from "../components/Jumbotron";
+import { Col, Row, Container } from "../components/Grid/index";
+import Hero from "../components/Hero/Hero";
+
 
 function NoMatch() {
   return (
     <Container fluid>
       <Row>
         <Col size="md-12">
-          <Jumbotron>
+          <Hero>
             <h1>404 Page Not Found</h1>
             <h1>
               <span role="img" aria-label="Face With Rolling Eyes Emoji">
                 🙄
               </span>
             </h1>
-          </Jumbotron>
+          </Hero>
         </Col>
       </Row>
     </Container>
