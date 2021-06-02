0eb229eea34540cf57ba378ca15e62d1ccb2685c
Ian Darland <iansmbpro@ians-mbp.lan>
2021-06-02 10:56:10 -0500

    stash

--- client/src/utils/geohook.js
+++ client/src/utils/geohook.js
@@ -1,12 +1,7 @@
 import Geocode from "react-geocode";
 
-<<<<<<< HEAD
-export function latLon() {
-    Geocode.fromAddress(`${user.address}`).then(
-=======
 function latLon(address) {
     Geocode.fromAddress(address).then(
->>>>>>> 41bbc66235031fd1767a13c43c6e038fbbee38c5
     (response) => {
       const { lat, lng } = response.results[0].geometry.location;
       console.log(lat, lng);
@@ -16,8 +11,5 @@ function latLon(address) {
     }
   )
 };
-<<<<<<< HEAD
-=======
 
 export default latLon;
->>>>>>> 41bbc66235031fd1767a13c43c6e038fbbee38c5
