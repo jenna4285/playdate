7c6b4a1340394197806b90916f4423a939902194
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-06-02 23:12:59 -0500

    half-working

--- client/src/components/YourFriends/YourFriends.js
+++ client/src/components/YourFriends/YourFriends.js
@@ -4,25 +4,37 @@ import UserContext from "../../utils/userContext";
 import { Chip } from 'primereact/chip';
 import { get } from "mongoose";
 import './YourFriends.css'
+import { useAuth0 } from "@auth0/auth0-react";
 
 
-function YourFriends() {
+function YourFriends(props) {
     const[users, setUsers]=useState()
-    const{dbUser}=useContext(UserContext)
+    const[friends, setFriends]=useState()
+    const { isAuthenticated, user } = useAuth0();
 
     useEffect(() => {
+     getUsers();
+     getFriends();
+    },[isAuthenticated]);
 
-       getUsers();
-       
-
-    },[]);
-        const getUsers=async()=>{
-   
-        const allFriends=await API.getUsers();        
-        console.log("My Friends");
-        console.log(allFriends.data);
-        setUsers(allFriends.data)
+    const getUsers=async()=>{
+        const allUsers=await API.getUsers();        
+        console.log("All Users");
+        console.log(allUsers.data);
+        setUsers(allUsers.data)
         }
+
+    const getFriends=async()=>{const myFriends = await API.getUserByEmail(props.data.email).then(userInfo => {
+            console.log(userInfo.data.friends)
+            setFriends(userInfo.data.friends);
+        }) ;       
+
+        console.log("All Friends");
+        console.log(myFriends.data);
+        setUsers(myFriends.data)
+        }
+
+
 
     return (
     <div className="col-sm-12 col-md-6 col-lg-6">
