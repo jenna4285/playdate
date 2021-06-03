57c1c0ed966367aa15b6eff4a9971ee5fd0961f8
Ian Darland <iansmbpro@ians-mbp.lan>
2021-06-03 15:39:25 -0500

    friend profiles working

--- client/src/components/KidCardContainer/KidCardContainer.js
+++ client/src/components/KidCardContainer/KidCardContainer.js
@@ -3,14 +3,13 @@ import Kidcard from "../Kidcard/Kidcard"
 import UserContext from "../../utils/userContext";
 
 
-function KidCardContainer() {
-    const {dbUser}=useContext(UserContext);
+function KidCardContainer(props) {
 
     return (
         <div className ="d-flex row">
         {/* This should be mapped over adding however many kids there are*/}
-        {dbUser.child ? (
-        dbUser.child.map((data) => (
+        {props.user.child ? (
+        props.user.child.map((data) => (
           <Kidcard key={data._id} name={data.kidname} age={data.kidage} gender={data.gender}/>
         ))
       ) : (
--- client/src/components/Usercard/Usercard.js
+++ client/src/components/Usercard/Usercard.js
@@ -1,20 +1,19 @@
 import React, {useContext} from "react";
 import UserContext from "../../utils/userContext"
 
-function Usercard (){
+function Usercard (props){
 
-  const {dbUser}=useContext(UserContext);
 
 
     return(
 <div className="card">
 <div className="card-body">
   <div className="d-flex flex-column align-items-center text-center">
-    <img src={dbUser.picture} alt="Admin" className="rounded-circle" width="150"/>
+    <img src={props.user.picture} alt="Admin" className="rounded-circle" width="150"/>
     <div className="mt-3">
-      <h4>{dbUser.fullname}</h4>
-      <p className="text-secondary mb-1">{dbUser.username}</p>
-      <p className="text-muted font-size-sm">{dbUser.description}</p>
+      <h4>{props.user.fullname}</h4>
+      <p className="text-secondary mb-1">{props.user.username}</p>
+      <p className="text-muted font-size-sm">{props.user.description}</p>
       <button className="btn btn-outline-primary">Message</button>
     </div>
   </div>
