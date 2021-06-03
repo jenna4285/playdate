97768bffe7318bf40216293f3766048d616aedd4
Jenna Garrison <78498827+jenna4285@users.noreply.github.com>
2021-06-03 14:37:24 -0500

    marker added and working with hard coded lat and lng and title.

--- client/src/components/Map/map.js
+++ client/src/components/Map/map.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { withGoogleMap, Map, GoogleApiWrapper, Circle} from 'google-maps-react';
+import { Marker, withGoogleMap, Map, GoogleApiWrapper, Circle} from 'google-maps-react';
 import UserContext from '../../utils/userContext';
 require('dotenv').config();
 
@@ -42,6 +42,11 @@ export class MapContainer extends Component {
       strokeWeight={5}
       fillColor='#FF0000'
       fillOpacity={0.2}
+    />
+    <Marker 
+      position={{lat: 30.2665, lng: -97.7688}}
+      map={MapContainer}
+      title="Test"
     />
     </Map>
     </div>
