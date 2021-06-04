593a63f612b015f41a98d5ecc470c89f6b59a11e
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-06-04 15:42:43 -0500

    centered activity card content

--- client/src/components/ActivityCard/ActivityCard.js
+++ client/src/components/ActivityCard/ActivityCard.js
@@ -8,7 +8,7 @@ function ActivityCard(props) {
         <div className="row">
             {props.activity.length ? (
                 props.activity.map((data) => (
-                    <div id="activity-container" className="activity-container  box p-d-flex p-mb-3 shadow">
+                    <div id="activity-container" className="activity-container justify-content-center p-d-flex p-mb-3 shadow">
                         <div className="row">
                             <div className="col-4">
                                 <h5>{data.location}</h5>
