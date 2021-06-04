fc02f76d4987f85cef074fc00a8f68115ea99ffb
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-06-04 13:43:39 -0500

    bolded kid card text

--- client/src/components/Kidcard/Kidcard.js
+++ client/src/components/Kidcard/Kidcard.js
@@ -6,7 +6,7 @@ function Kidcard(props) {
 <div className="col-sm-12 col-md-4 col-lg-4">
       <div className="card h-100 kid-card ">
         <div className="card-body">
-          <h6 className="d-flex align-items-center mb-3">{props.name}</h6>
+          <h6 className="d-flex align-items-center mb-3 kid-name">{props.name}</h6>
           <small>Age</small>
           <div>{props.age}</div>
           <small>Gender</small>
