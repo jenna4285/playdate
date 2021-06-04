e17ad750616e6c178fd5f27a67ba4ef814b142f7
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-06-04 13:21:55 -0500

    styled kid cards on dashboard

--- client/src/components/Kidcard/Kidcard.js
+++ client/src/components/Kidcard/Kidcard.js
@@ -3,7 +3,7 @@ import './Kidcard.css'
 
 function Kidcard(props) {
   return (
-<div className="col-sm-12 col-md-6 col-lg-4">
+<div className="col-sm-12 col-md-4 col-lg-4">
       <div className="card h-100 kid-card ">
         <div className="card-body">
           <h6 className="d-flex align-items-center mb-3">{props.name}</h6>
