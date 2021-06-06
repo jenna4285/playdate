84d944c967d34756744116aaf876eb722d8ec24a
Jenna Garrison <78498827+jenna4285@users.noreply.github.com>
2021-06-06 09:53:08 -0500

    updated classnames to get font color updated on cards

--- client/src/components/HomeInfo/HomeInfo.js
+++ client/src/components/HomeInfo/HomeInfo.js
@@ -10,24 +10,24 @@ function HomeInfo(){
     <div class="card">
       <img src="https://i.imgur.com/MmDUP78.jpg?2" class="card-img-top" alt="Person on device"/>
       <div class="card-body">
-        <h5 class="card-title">Connect!</h5>
-        <p class="card-text">Utilize PlayDate to connect with other families near you.</p>
+        <h5 class="card-title home-card">Connect!</h5>
+        <p class="card-text home-card">Utilize PlayDate to connect with other families near you.</p>
       </div>
       {/* <div class="card-footer"></div> */}
     </div>
     <div class="card">
       <img src="https://i.imgur.com/c9iY1Yj.jpg?2" class="card-img-top" alt="Children Playing Tug of War"/>
       <div class="card-body">
-        <h5 class="card-title">Play!</h5>
-        <p class="card-text">See what your neighbors have planned and join the fun.</p>
+        <h5 class="card-title home-card">Play!</h5>
+        <p class="card-text home-card">See what your neighbors have planned and join the fun.</p>
       </div>
       {/* <div class="card-footer"></div> */}
     </div>
     <div class="card">
       <img src="https://i.imgur.com/hhloLYp.jpg" class="card-img-top" alt="Kids Playing Spike Ball"/>
       <div class="card-body">
-        <h5 class="card-title">Invite!</h5>
-        <p class="card-text">Add your upcoming activities and connect with your neighbors.</p>
+        <h5 class="card-title home-card">Invite!</h5>
+        <p class="card-text home-card">Add your upcoming activities and connect with your neighbors.</p>
       </div>
       {/* <div class="card-footer"></div> */}
     </div>
