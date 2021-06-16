fa4cea3d650a129056e527c90c4fd402dda3460f
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-06-16 00:09:31 -0500

    arranged activity cards into columns. Need to make them more responsive (rows)

--- client/src/components/ActivityCard/ActivityCard.js
+++ client/src/components/ActivityCard/ActivityCard.js
@@ -1,47 +1,59 @@
-import React, {useContext} from "react";
-import API from "../../utils/API";
-import UserContext from "../../utils/userContext"
-import { Link } from "react-router-dom"
+import React, { useContext } from 'react';
+import API from '../../utils/API';
+import UserContext from '../../utils/userContext';
+import { Link } from 'react-router-dom';
+import { Chip } from 'primereact/chip';
 
 function ActivityCard(props) {
+	const { dbUser } = useContext(UserContext);
 
-    const { dbUser } = useContext(UserContext);
-
-    return (
-
-        <div className="row">
-            {props.activity.length ? (
-                props.activity.map((data) => (
-                    <div key={data._id} id="activity-container" className="activity-container justify-content-center p-d-flex p-mb-3 shadow">
-                        <div className="row">
-                            <div className="col-4">
-                                <h5 className='mt-3'>{data.location}</h5>
-                            </div>
-                            <div className="col-4">
-                            <h5 className='mt-3'>{new Date(data.date).toLocaleDateString()}</h5>
-                            </div>
-                            <div className="col-4">
-
-                                <h5 className='mt-3'>Host: <Link to={"/" + data.hostId}>{data.hostName}</Link></h5>
-
-                            </div>
-                            <div className="row justify-content-center">
-                                <p style={{ textAlign: "center" }}>{data.description}</p>
-                            </div>
-                            {data.hostId === dbUser._id?
-                            <div className="d-flex justify-content-center">
-                                <button className="btn btn-danger" name= {data._id} onClick={props.deleteActivity}>Delete Your Activity</button>
-                            </div>
-                            : null}
-                        </div>
-                    </div>
-                ))
-            ) : (
-                null
-            )}
-
-        </div>
-    )
+	return (
+		<div className="row">
+			{props.activity.length ? (
+				props.activity.map((data) => (
+					<div key={data._id} id="activity-container" className="activity-container p-mb-3 shadow">
+						<div className="row">
+							<div className="row date">
+								<h5 className="mt-3">{new Date(data.date).toLocaleDateString()}</h5>
+							</div>
+							<div className="col-3">
+								<h7 className="mt-3">{data.location}</h7>
+							</div>
+							<div className="col-3">
+								<h7 className="mt-3">
+									Host:{' '}
+									<Link className="no-dec" to={'/' + data._id}>
+										<Chip
+											key={data.id}
+											label={data.hostName}
+											className="friend-chip"
+                                            style={{backgroundColor:"#1dbbd3"}}
+										/>
+									</Link>
+								</h7>
+							</div>
+							<div className="col-6">
+								<p style={{ textAlign: 'center', verticalAlign: 'center' }}>{data.description}</p>
+							</div>
+							{data.hostId === dbUser._id ? (
+								<div className="row d-flex justify-content-center">
+									<button className="btn btn-danger" name={data._id} onClick={props.deleteActivity}>
+										Delete Your Activity
+									</button>
+								</div>
+							) : (
+								<div className="row d-flex justify-content-center">
+									<button className="btn btn-success " name={data._id} onClick={props.attendActivity}>
+										Attend This Activity!
+									</button>
+								</div>
+							)}
+						</div>
+					</div>
+				))
+			) : null}
+		</div>
+	);
 }
 
 export default ActivityCard;
