fcf80275cb9ee167f82079483cc6bb437e359bc8
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-06-16 00:21:23 -0500

    got delete and attend activity  buttons to center and fit content

--- client/src/components/ActivityCard/ActivityCard.js
+++ client/src/components/ActivityCard/ActivityCard.js
@@ -3,6 +3,7 @@ import API from '../../utils/API';
 import UserContext from '../../utils/userContext';
 import { Link } from 'react-router-dom';
 import { Chip } from 'primereact/chip';
+import './ActivityCard.css'
 
 function ActivityCard(props) {
 	const { dbUser } = useContext(UserContext);
@@ -36,13 +37,13 @@ function ActivityCard(props) {
 								<p style={{ textAlign: 'center', verticalAlign: 'center' }}>{data.description}</p>
 							</div>
 							{data.hostId === dbUser._id ? (
-								<div className="row d-flex justify-content-center">
+								<div className="row justify-content-center">
 									<button className="btn btn-danger" name={data._id} onClick={props.deleteActivity}>
 										Delete Your Activity
 									</button>
 								</div>
 							) : (
-								<div className="row d-flex justify-content-center">
+								<div className="row justify-content-center">
 									<button className="btn btn-success " name={data._id} onClick={props.attendActivity}>
 										Attend This Activity!
 									</button>
