eb75608df80bcb67c3ffd3ed31afa28bf1777cab
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-06-23 21:43:22 -0500

    added leaflet map to Dashboard and Activity page, replaed google map

---
+++ client/src/components/MiniLeafletMap/MiniLeafletMap.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import './MiniLeafletMap.css'
+
+
+let myIcon = L.icon({
+	iconUrl: './playdateMarker2.png',
+	iconSize: [ 44, 44 ],
+	iconAnchor: [ 22, 94 ],
+	popupAnchor: [ -3, -76 ]
+});
+
+let homeIcon = L.icon({
+	iconUrl: './playground.png',
+	iconSize: [ 50, 50 ],
+	iconAnchor: [ 22, 94 ],
+	popupAnchor: [ -3, -76 ]
+});
+
+
+function MiniLeafletMap(props) {
+	return (
+		<div id="map-div">
+		
+			<MapContainer
+                id="dashboard-map"
+				center={[ props.activity.actLat, props.activity.actLng ]}
+				zoom={13}
+				scrollWheelZoom={false}
+				// style={{ height: '50vh', width: '100wh' }}
+			>
+				<TileLayer
+					attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+				{/* <Marker position={[props.dbUser.lat, props.dbUser.lng]} icon={homeIcon}>
+					<Popup>
+						<h6>Your Address!</h6> <br /> There's no place like home!
+					</Popup>
+				</Marker> */}
+						<Marker position={[ props.activity.actLat, props.activity.actLng ]}>
+							<Popup>
+								<h6>{props.activity.hostName}'s Activity</h6>
+                                <br/>
+								<h6>Date: {new Date(props.activity.date).toLocaleDateString()}</h6>
+                                <br/>
+                               	{props.activity.description} 
+                                <br/>
+                                <br/>
+							    {props.activity.attendees.length} attendee(s) so far!
+							</Popup>
+						</Marker>
+				</MapContainer>
+		</div>
+	);
+}
+
+export default MiniLeafletMap;
--- client/src/pages/ActivityPage.js
+++ client/src/pages/ActivityPage.js
@@ -1,75 +1,71 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import UserContext from "../utils/userContext";
-import "../pages/Profile.css";
+import UserContext from '../utils/userContext';
+import '../pages/Profile.css';
 import { Chip } from 'primereact/chip';
-import Messages from "../components/Messages/Messages";
-import "./ActivityPage.css";
-import API from "../utils/API";
-
+import Messages from '../components/Messages/Messages';
+import './ActivityPage.css';
+import API from '../utils/API';
+import MiniLeafletMap from '../components/MiniLeafletMap/MiniLeafletMap';
 
 
 function ActivityPage() {
-  const { dbUser } = useContext(UserContext);
-  const { id } = useParams()
-  const [activityInfo, setActivityInfo] = useState({})
+	const { dbUser } = useContext(UserContext);
+	const { id } = useParams();
+	const [ activityInfo, setActivityInfo ] = useState({});
 
-  useEffect(()=> {
-	  getActivity(id)
-	  console.log(activityInfo)
-  }, []) 
+	useEffect(() => {
+		getActivity(id);
+		console.log(activityInfo);
+	}, []);
 
-  const getActivity = (data) => {
-	  API.getActivityById(data)
-	  .then((res) => setActivityInfo(res.data))
-  }
+	const getActivity = (data) => {
+		API.getActivityById(data).then((res) => setActivityInfo(res.data));
+	};
 
-  return (
-    <div className="text-center">
-      <div
-        id="activity-page-card"
-        className="card activity-card d-flex container"
-      >
-        <div class="row">
-          <h1>Activity Title</h1>
-          <h4>Host</h4>
-          <h4>Lorem ipsum dolorem sipsum </h4>
-          <hr />
-        </div>
-        <div id="map-attendees" class="row">
-          <div class="col">
-            <h1>MAP GOES HERE</h1>
-          </div>
-          <div class="col">
-            <div className="card">
-              <h1>Whos Going</h1>
-              {activityInfo.attendees?
-			  	activityInfo.attendees[0]? (
-                activityInfo.attendees.map((item) => (
-                  <div>
-                    <Link className="no-dec" to={"/profile/" + item._id}>
-                      <Chip
-                        key={item._id}
-                        label={item.fullname}
-                        image={item.picture}
-                        className="friend-chip"
-                      />
-                    </Link>
-                  </div>
-                ))
-              ) : (
-                <h2>Be the First to RSVP</h2>
-              ) :(null)}
-            </div>
-          </div>
-        </div>
-        <hr />
-        <div id="comments-row" className="row">
-          <button className="btn btn-success">Post Comment</button>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="text-center d-flex container">
+			<div id="activity-page-card" className="card activity-card d-flex container">
+				<div class="row">
+					<h1>Activity Title</h1>
+					<h4>{activityInfo.hostName}'s Activity</h4>
+					<h4>{activityInfo.description}</h4>
+					<hr />
+				</div>
+				<div id="map-attendees" class="row">
+					<div class="col">
+					{dbUser.fullname && activityInfo.attendees ? <MiniLeafletMap activity={activityInfo} dbUser={dbUser} /> : null}
+					
+					</div>
+					<div class="col">
+						<div>
+							<h1>Whos Going</h1>
+							{activityInfo.attendees ? activityInfo.attendees[0] ? (
+								activityInfo.attendees.map((item) => (
+									<div>
+										<Link className="no-dec" to={'/profile/' + item._id}>
+											<Chip
+												key={item._id}
+												label={item.fullname}
+												image={item.picture}
+												className="friend-chip"
+											/>
+										</Link>
+									</div>
+								))
+							) : (
+								<h2>Be the First to RSVP</h2>
+							) : null}
+						</div>
+					</div>
+				</div>
+				<hr />
+				<div id="comments-row" className="row">
+					<button className="btn btn-success">Post Comment</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default ActivityPage;
