cd0e52ec0794af4af1825f49e4ab547c374334eb
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-07-21 14:34:57 -0500

    chips and distances now showing side by side

--- client/src/components/YourFriends/YourFriends.js
+++ client/src/components/YourFriends/YourFriends.js
@@ -1,101 +1,114 @@
-import React, { useState, useEffect, useContext } from "react";
-import API from "../../utils/API";
-import UserContext from "../../utils/userContext";
-import SearchBar from "../SearchBar";
-import { Link } from "react-router-dom"
+import React, { useState, useEffect, useContext } from 'react';
+import API from '../../utils/API';
+import UserContext from '../../utils/userContext';
+import SearchBar from '../SearchBar';
+import { Link } from 'react-router-dom';
 import { Chip } from 'primereact/chip';
-import './YourFriends.css'
-import { useAuth0 } from "@auth0/auth0-react";
+import './YourFriends.css';
+import { useAuth0 } from '@auth0/auth0-react';
 import { ScrollPanel } from 'primereact/scrollpanel';
 
-
-
 function YourFriends(props) {
-  const { dbUser } = useContext(UserContext)
-  const [users, setUsers] = useState()
-  const [filteredUsers, setFilteredUsers] = useState([])
-  const { isAuthenticated, user } = useAuth0();
+	const { dbUser } = useContext(UserContext);
+	const [ users, setUsers ] = useState();
+	const [ filteredUsers, setFilteredUsers ] = useState([]);
+	const { isAuthenticated, user } = useAuth0();
 
-  //current user's lat and lon
-  const lat1 = dbUser.lat
-  const lon1 = dbUser.lng
+	//current user's lat and lon
+	const lat1 = dbUser.lat;
+	const lon1 = dbUser.lng;
 
+	useEffect(() => {
+		getUsers();
+		//  getFriends();
+	}, []);
 
-  useEffect(() => {
-    getUsers();
-    //  getFriends();
-  }, []);
+	const getUsers = async () => {
+		const allUsers = await API.getUsers();
+		setUsers(allUsers.data);
+		setFilteredUsers(allUsers.data);
+	};
 
-  const getUsers = async () => {
-    const allUsers = await API.getUsers();
-    setUsers(allUsers.data)
-    setFilteredUsers(allUsers.data)
-  }
+	const handleChange = (e) => {
+		const searchField = e.target.value;
+		const userSearch = users.filter(
+			(item) =>
+				!item.fullname
+					? item.email.toLowerCase().includes(searchField.toLowerCase())
+					: item.fullname.toLowerCase().includes(searchField.toLowerCase())
+		);
+		setFilteredUsers(userSearch);
+	};
 
-  const handleChange = (e) => {
-    const searchField = e.target.value
-    const userSearch = users.filter(item => 
-      !item.fullname ?
-      item.email.toLowerCase().includes(searchField.toLowerCase())
-      :
-      item.fullname.toLowerCase().includes(searchField.toLowerCase())
-  )
-    setFilteredUsers(userSearch)
-  }  
+	// const getFriends=async()=>{const myFriends = await API.getUserByEmail(user.email).then(userInfo => {
+	//         console.log(userInfo.data.friends)
+	//         setFriends(userInfo.data.friends);
+	//     }) ;
 
-  // const getFriends=async()=>{const myFriends = await API.getUserByEmail(user.email).then(userInfo => {
-  //         console.log(userInfo.data.friends)
-  //         setFriends(userInfo.data.friends);
-  //     }) ;       
+	//     console.log("All Friends");
+	//     console.log(myFriends.data);
+	//     setUsers(myFriends.data)
+	//     }
 
-  //     console.log("All Friends");
-  //     console.log(myFriends.data);
-  //     setUsers(myFriends.data)
-  //     }
-
-
-  return (
-    <div className="col-sm-12 col-md-6 col-lg-6">
-      <div className='row no-gut'>
-        <div className="card">
-          <h1>Your Friends</h1>
-          <ScrollPanel style={{width: '100%', height: 'min-content'}}>
-          {dbUser.friends ? (
-            dbUser.friends.map((item) => (
-              <div>
-                <Chip />
-                <Link className='no-dec' to={"/profile/" + item._id}><Chip key={item._id} label={item.fullname} image={item.picture} className="friend-chip" /></Link> <Chip label={props.distance(lat1, lon1,item.lat, item.lng, "M")}  className="friend-chip" />
-              </div>
-            ))
-          ) : (
-            <p>""</p>
-          )}
-          </ScrollPanel>
-        </div>
-      </div>
-      <div className="row no-gut">
-        <div className="card">
-          <h1>Your Neighbors</h1>
-          <SearchBar handleChange = {handleChange} />
-          <ScrollPanel style={{width: '100%', height: '200px'}}>
-          {filteredUsers ? (
-            filteredUsers.map((item) => (
-              item._id !== dbUser._id ?
-              <div key={item._id}>
-                <Link className='no-dec' to={"/profile/" + item._id}><Chip label={item.fullname} image={item.picture} className="friend-chip" /></Link>
-              </div>
-              : null
-            ))
-          ) : (
-            <p>""</p>
-          )}
-          </ScrollPanel>
-        </div>
-      </div>
-    </div>
-
-  )
-
-
+	return (
+		<div className="col-sm-12 col-md-6 col-lg-6">
+			<div className="row no-gut">
+				<div className="card">
+					<h1>Your Friends</h1>
+					<ScrollPanel style={{ width: '100%', height: 'min-content' }}>
+						{dbUser.friends ? (
+							dbUser.friends.map((item) => (
+								<div>
+									<Chip />
+									<Link className="no-dec" to={'/profile/' + item._id}>
+										<Chip
+											key={item._id}
+											label={item.fullname}
+											image={item.picture}
+											style={{ display: 'inline' }}
+											className="friend-chip"
+										/>
+									</Link>{' '}
+									<Chip
+										style={{ display: 'inline' }}
+										label={props.distance(lat1, lon1, item.lat, item.lng, 'M')}
+										className="friend-chip"
+									/>
+								</div>
+							))
+						) : (
+							<p>""</p>
+						)}
+					</ScrollPanel>
+				</div>
+			</div>
+			<div className="row no-gut">
+				<div className="card">
+					<h1>Your Neighbors</h1>
+					<SearchBar handleChange={handleChange} />
+					<ScrollPanel style={{ width: '100%', height: '200px' }}>
+						{filteredUsers ? (
+							filteredUsers.map(
+								(item) =>
+									item._id !== dbUser._id ? (
+										<div key={item._id}>
+											<Link className="no-dec" to={'/profile/' + item._id}>
+												<Chip
+													label={item.fullname}
+													image={item.picture}
+													className="friend-chip"
+												/>
+											</Link>
+										</div>
+									) : null
+							)
+						) : (
+							<p>""</p>
+						)}
+					</ScrollPanel>
+				</div>
+			</div>
+		</div>
+	);
 }
 export default YourFriends;
