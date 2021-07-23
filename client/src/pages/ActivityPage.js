712adfa6c7c6c24daf42ffb9fd36e659dea14eb3
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-07-23 16:12:06 -0500

    added title and date to activity pages

--- client/src/pages/ActivityPage.js
+++ client/src/pages/ActivityPage.js
@@ -73,11 +73,34 @@ function ActivityPage() {
 		.then(setAttending(true))
 	}
 
+	function calculateDistance(lat1, lon1, lat2, lon2, unit) {
+		if ((lat1 == lat2) && (lon1 == lon2)) {
+			return 0;
+		}
+		else {
+			var radlat1 = Math.PI * lat1/180;
+			var radlat2 = Math.PI * lat2/180;
+			var theta = lon1-lon2;
+			var radtheta = Math.PI * theta/180;
+			var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+			if (dist > 1) {
+				dist = 1;
+			}
+			dist = Math.acos(dist);
+			dist = dist * 180/Math.PI;
+			dist = dist * 60 * 1.1515;
+			if (unit=="K") { dist = dist * 1.609344 }
+			if (unit=="N") { dist = dist * 0.8684 }
+			return (Math.round(dist * 100) / 100)
+		}
+	}
+
 	return (
 		<div className="text-center d-flex container">
 			<div id="activity-page-card" className="card activity-card d-flex container">
 				<div class="row">
-					<h1>Activity Title</h1>
+					<h1>{activityInfo.title}</h1>
+					<h5>{calculateDistance(dbUser.lat, dbUser.lng, activityInfo.actLat, activityInfo.actLng, "M")} Miles Away</h5>
 					<h4>{activityInfo.hostName}'s Activity</h4>
 					<h4>{activityInfo.description}</h4>
 					<hr />
